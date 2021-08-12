2f8d5e6ecde62144bb0f0cb6af61cc152c91447c
aglowbuffalo <popcorn44@abv.bg>
2021-08-12 08:10:13 +0300

    Signed-off-by: aglowbuffalo <popcorn44@abv.bg>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
     <div class="home-text">First method : at first I wanted to get data once from Api and then just to pass it to controller (to escape special characters) when I need the next page.<br>
+        PS: After I send the task I check my php setting and saw that data transfer is to little so the first method where i slice the array and pass it to controller work normal.
         It work but php give me warning for to much data so I change it to when I need next page just get the data from api(did't see pagination in the api so I get all the data)
     </div>
     <div class="home-text">Second method : get data from api save it in DB and then use it
